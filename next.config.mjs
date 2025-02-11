b6a5b4e7cc75352f100ed0666488756f194b73ac
NooberThanYall <nooberthanyall@gmail.com>
2025-02-11 15:48:38 +0300

    h

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['wzaevxnrrqabamgkdxcb.supabase.co']
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
