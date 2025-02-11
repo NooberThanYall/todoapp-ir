b21524390077b72bf75bae315993f8cb810eb710
NooberThanYall <nooberthanyall@gmail.com>
2025-02-11 16:02:52 +0300

    huh

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,29 +2,34 @@ import localFont from "next/font/local";
 import "../globals.css";
 import { Navbar } from "../components/navbar";
 import Footer from "../components/footer";
+import { Metadata } from "next";
 
 const estedadMed = localFont({
-  src: "../fonts/Estedad-Medium.ttf",
+    src: "../fonts/Estedad-Medium.ttf",
 });
- const estedadBold = localFont({
-  src: "../fonts/Estedad-Bold.ttf",
+const estedadBold = localFont({
+    src: "../fonts/Estedad-Bold.ttf",
 });
 export const bold = estedadBold.className;
-
+export const metadata: Metadata = {
+    title: "hello",
+    description: "Fuck Your Ass",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fa" dir="rtl" className="dark">
-      <body className={`${estedadMed.className} antialiased overflow-x-hidden`}>
-        <Navbar/>
-        <main className=" w-3/4 mx-auto h-screen">{children}</main>
-        <Footer />
-        
-      </body>
-    </html>
-  );
+    return (
+        <html lang="fa" dir="rtl" className="dark">
+            <body
+                className={`${estedadMed.className} antialiased overflow-x-hidden`}
+            >
+                <Navbar />
+                <main className=" w-3/4 mx-auto h-screen">{children}</main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
